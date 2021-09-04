63da6d0e9bdeb3595d960c537ca0bf73b0249713
Andrew Smith <ajs3993@gmail.com>
2021-09-04 16:21:20 -0500

    task component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 const App = (props)  => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'meeting',
+        day: 'Feb 6th at 1:30',
+        reminder: false
+    }
+]
+)
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,12 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'meeting',
-        day: 'Feb 6th at 1:30',
-        reminder: false
-    }
-]
+import {useState} from 'react'
+import Task from './Task'
 
 
-const Tasks = () => {
+const Tasks = ({tasks, onDelete}) => {
     return (
         <>
             {tasks.map((task) => (
-            <h3 key ={task.id}>{task.text}</h3>
+            <Task key ={task.id} task={task}/>
             ))}
         </>
     )
